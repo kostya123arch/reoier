09c32defb270b4d515f9710320b019bc24efdf05
kostya123arch <const.andr.2007@gmail.com>
2025-09-08 14:08:10 +0300

    1.cpp

--- main.cpp
+++ main.cpp
@@ -9,3 +9,27 @@ int main() {
     cout << endl;
     for (int i = 1; i < s.length(); i+=2) cout << s[i];
 }
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    vector<int> v;
+    cin >> n;
+    int b;
+ 
+for (int i = 0; i < n; i++){
+    getline(cin, b);
+    v.push_back(n);
+}
+    sort(begin(v), end(v));
+    for (int i = 0; i < v.size(); i++){
+        cout << v[i];
+    }
+    
+    return 0;
+}
+
+
